0003f2c69876dd48f605bc6c54caf8a9fcc0f9fb
maskenary <maskenary@gmail.com>
2024-05-06 17:05:48 -0700

    changes to charged applestick - buff strength and duration depends on charges gained by hitting living entities - maintains glowing on player when holding - adds tooltip description

--- src/main/java/net/maskenary/applestick/item/custom/ChargedAppleStickItem.java
+++ src/main/java/net/maskenary/applestick/item/custom/ChargedAppleStickItem.java
@@ -1,5 +1,7 @@
 package net.maskenary.applestick.item.custom;
 
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -8,20 +10,38 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 
+import java.util.List;
+
 
 public class ChargedAppleStickItem extends Item {
+    public int lifeforce;
     public ChargedAppleStickItem(Settings settings) {
         super(settings);
+        lifeforce = 0;
     }
 
     @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
         if (!world.isClient){
-            user.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 400, 2));
-            user.addStatusEffect(new StatusEffectInstance(StatusEffects.HASTE, 400, 2));
-            user.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 400));
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 20*(lifeforce/5)+1, lifeforce/25));
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.HASTE, 20*(lifeforce/5)+1, lifeforce/25));
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 20*(lifeforce/5)+1));
             user.sendMessage(Text.literal("You feel invigorated as a surge of energy courses through your veins!"));
         }
-        return stack;
+        return super.finishUsing(stack, world, user);
+    }
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker){
+        lifeforce++;
+        return super.postHit(stack, target, attacker);
+    }
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (!world.isClient){
+            ((LivingEntity)entity).addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 2));
+        }
+    }
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        tooltip.add(Text.of("Hitting living beings gathers Life Force into this item to increase its buff"));
+        tooltip.add(Text.of("§e§l Current Life Force Gathered: " + lifeforce + "§e§l"));
     }
 }
